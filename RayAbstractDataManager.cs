966322f7ce880d233a1bee876b205f6282ea17b6
AlteNielsen <alte.nielsen@gmail.com>
2026-03-05 16:49:11 +0900

    FirstUpload

---
+++ RayAbstractDataManager.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Ray.Data
+{
+    public abstract class RayAbstractDataManager : MonoBehaviour
+    {
+        public static RayAbstractDataManager Instance;
+        private float[] masterBuffer;
+        private RayDataStruct[] dataInterfaces;
+        private int[] offsets;
+
+        private List<float[]> datalist = new();
+        private bool RegisterSwitch;
+        private int dICounter;
+
+        public void GetData(int selector, float[] frame)
+        {
+            Array.Copy(masterBuffer, offsets[selector], frame, 0, frame.Length);
+        }
+
+        public void SetData(int selector, float[] values)
+        {
+            Array.Copy(values, 0, masterBuffer, offsets[selector], values.Length);
+        }
+
+        public T Access<T>(int key) where T : struct, RayDataStruct
+        {
+            return (T)dataInterfaces[key];
+        }
+
+        protected void Save()
+        {
+            for(int i = 0; i < dataInterfaces.Length; i++)
+            {
+                dataInterfaces[i].Save();
+            }
+        }
+
+        void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+            RegisterSwitch = false;
+            StructRegistry();
+            dataInterfaces = new RayDataStruct[datalist.Count];
+            RegisterSwitch = true;
+            StructRegistry();
+            CompileData();
+        }
+
+        protected abstract void StructRegistry();
+
+        protected void Register<T>() where T : struct, RayDataStruct
+        {
+            if(!RegisterSwitch)
+            {
+                datalist.Add(new T().Load());
+            }
+            else
+            {
+                dataInterfaces[dICounter] = new T();
+                dICounter++;
+            }
+        }
+
+        private void CompileData()
+        {
+            int[] lengths = new int[datalist.Count];
+            for(int i = 0; i < lengths.Length; i++)
+            {
+                lengths[i] = datalist[i].Length;
+            }
+            masterBuffer = new float[lengths.Sum()];
+            Memory<float> masterMemory = masterBuffer.AsMemory();
+
+            int currentOffset = 0;
+            offsets = new int[lengths.Length];
+            for(int i = 0; i < lengths.Length;  i++)
+            {
+                Array.Copy(datalist[i], 0, masterBuffer, currentOffset, lengths[i]);
+                dataInterfaces[i].data = masterMemory.Slice(currentOffset, lengths[i]);
+                offsets[i] = currentOffset;
+                currentOffset += lengths[i];
+            }
+            datalist = null;
+        }
+    }
+
+    public interface RayDataStruct
+    {
+        public ReadOnlyMemory<float> data { get; set; }
+
+        public void Save();
+        public float[] Load();
+    }
+}
